6fdc68149e30b2844b5dff334941f451b180bd55
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-09-13 22:56:12 -0300

    Update ex-02.c

--- Lista 1/ex-02.c
+++ Lista 1/ex-02.c
@@ -1,14 +1,20 @@
 /*
-Declare um array de 5 caracteres e inicialize-o com as
-letras: ’a’, ’b’, ’c’, ’d’ e ’e’. Imprima os elementos do array.
+Declare um array de 5 caracteres e inicialize-o com as letras
+'a', 'b', 'c', 'd' e 'e'. Imprima os elementos do array.
 */
 
 #include <stdio.h>
-int main(){
+
+int main()
+{
     char array[5] = {'a', 'b', 'c', 'd', 'e'};
+
     printf("Elementos do array:\n");
-    for(int i=0; i<5; i++){
+
+    for (int i=0; i<5; i++)
+    {
         printf("%c\n", array[i]);
     }
+
     return 0;
 }
