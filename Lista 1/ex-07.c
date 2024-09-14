89326ea14ca81bb6978f7e8dca9a4daf76eb310c
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-09-13 22:57:58 -0300

    Update ex-07.c

--- Lista 1/ex-07.c
+++ Lista 1/ex-07.c
@@ -1,16 +1,23 @@
 /*
 Escreva um programa em C que leia 6 valores de ponto flutuante
-e calcule a média dos valores.
+e calcule a mÃ©dia dos valores.
 */
 
 #include <stdio.h>
-int main(){
+
+int main()
+{
     float num, soma=0;
-    for(int i=0; i<6; i++){
+
+    for (int i=0; i<6; i++)
+    {
         printf("Digite o %do valor: ", i+1);
         scanf("%f",&num);
+
         soma += num;
     }
+
     printf("Media: %f\n", soma/6);
+
     return 0;
 }
