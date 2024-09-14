99eb1763d3e33a54ed9512f4ced03e9677444b17
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-09-13 22:57:40 -0300

    Update ex-06.c

--- Lista 1/ex-06.c
+++ Lista 1/ex-06.c
@@ -4,13 +4,20 @@ calcule a soma de todos os elementos do array.
 */
 
 #include <stdio.h>
-int main(){
+
+int main()
+{
     int valor[10], soma=0, i;
-    for(i=0; i<10; i++){
+
+    for (i=0; i<10; i++)
+    {
         printf("Digite o %do valor: ", i+1);
         scanf("%d",&valor[i]);
+
         soma += valor[i];
     }
+
     printf("Soma: %d\n", soma);
+
     return 0;
 }
