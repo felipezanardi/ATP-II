bf299ddaf66f20278480aae120b7427ab8f99404
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-09-13 22:58:30 -0300

    Update ex-08.c

--- Lista 1/ex-08.c
+++ Lista 1/ex-08.c
@@ -1,25 +1,34 @@
 /*
 Escreva um programa em C que leia 8 caracteres e verifique
-quantos s„o vogais.
+quantos s√£o vogais.
 */
 
 #include <stdio.h>
-int main(){
+
+int main()
+{
     char letra;
     int vogal=0, i;
-    for(i=0; i<8; i++){
+
+    for (i=0; i<8; i++)
+    {
         printf("Digite o %do caracter: ", i+1);
         scanf("%c",&letra);
         fflush(stdin);
-        switch(letra){
+
+        switch(letra)
+        {
             case 'a':case 'e':case 'i':case 'o':case 'u':
             case 'A':case 'E':case 'I':case 'O':case 'U':
                 vogal++;
                 break;
+
             default:
                 break;
         }
     }
+
     printf("Vogais: %d\n", vogal);
+
     return 0;
 }
