69941be801fcba37ba79b5d609ff913ed7f466e5
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-09-13 22:55:40 -0300

    Update ex-01.c

--- Lista 1/ex-01.c
+++ Lista 1/ex-01.c
@@ -4,11 +4,17 @@ Escreva um programa em C que imprima todos os elementos do array.
 */
 
 #include <stdio.h>
-int main(){
+
+int main()
+{
     int vetor[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
     printf("Elementos do array:\n");
-    for(int i=0; i<10; i++){
+
+    for (int i=0; i<10; i++)
+    {
         printf("%d\n", vetor[i]);
     }
+    
     return 0;
 }
