9b53e39f513521c61815b0488a6272c616fab79a
Felipe Zanardi <101890401+felipezanardi@users.noreply.github.com>
2024-09-13 22:57:03 -0300

    Update ex-04.c

--- Lista 1/ex-04.c
+++ Lista 1/ex-04.c
@@ -1,15 +1,21 @@
 /*
-Declare um array de 8 números de ponto flutuante e inicialize-o
-com os valores dos primeiros 8 números da sequência de Fibonacci.
+Declare um array de 8 nÃºmeros de ponto flutuante e inicialize-o
+com os valores dos primeiros 8 nÃºmeros da sequÃªncia de Fibonacci.
 Imprima todos os elementos do array.
 */
 
 #include <stdio.h>
-int main(){
+
+int main()
+{
     float array[8] = {1, 1, 2, 3, 5, 8, 13, 21};
+
     printf("Elementos do array:\n");
-    for(int i=0; i<8; i++){
+
+    for (int i=0; i<8; i++)
+    {
         printf("%f\n", array[i]);
     }
+
     return 0;
 }
